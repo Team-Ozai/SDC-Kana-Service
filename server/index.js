79da4548efb5bd1013500799457492dead4bfd81
Kana-E <kana.eguchi@yahoo.com>
2020-07-08 10:19:19 -0400

    adds API for additional CRUD operations for Banner

--- server/index.js
+++ server/index.js
@@ -12,25 +12,16 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-// app.get('/:id', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
-// });
-// for proxy server
 
+//////// Banner Data ////////
+//get any banner data
 app.get('/api/banner', (req, res) => {
   db.Banner.findOne()
   .then(result => {
     res.send(result);
   })
 });
-
-app.get('/api/banners', (req, res) => {
-  db.Banner.findAll()
-  .then(result => {
-    res.send(result);
-  })
-});
-
+//get specific banner data
 app.get('/api/banner/:bannerId', (req, res) => {
   db.Banner.findOne({where: {id: req.params.bannerId}})
   .then(result => {
@@ -38,13 +29,43 @@ app.get('/api/banner/:bannerId', (req, res) => {
   })
 });
 
-app.get('/api/videos', (req, res) => {
-  db.Video.findAll()
+//get all banner data -> unnecessary?
+app.get('/api/banners', (req, res) => {
+  db.Banner.findAll()
   .then(result => {
     res.send(result);
   })
 });
 
+//post 5 more banner data
+app.post('/api/banners', (req, res) => {
+  db.generateBanners()
+  .then(db.Banner.findAll())
+    .then(result => {
+      res.send(result);
+    });
+})
+
+//increments pledged amount by 10
+app.patch('/api/banner/:bannerId', (req, res) => {
+  db.BannerfindOne({where: {id: req.params.bannerId}})
+    .then(result.increment('amount_pledged', {by : 10}))
+      .then(result => {
+        res.send(result);
+      })
+})
+
+
+//deletes one banner
+app.delete('/api/banner/:bannerId', (req, res) => {
+  db.BannerfindOne({where: {id: req.params.bannerId}})
+    .then(result => result.destroy())
+      .catch(err => console.log(err))
+        .then(res.end());
+
+})
+
+//////*Video Data*//////
 app.get('/api/video/:videoId', (req, res) => {
   console.log(req.params.videoId);
   db.Video.findOne({where: {id: req.params.videoId}})
@@ -55,6 +76,13 @@ app.get('/api/video/:videoId', (req, res) => {
 
 app.get('/api/video', (req, res) => {
   db.Video.findOne()
+  .then(result => {
+    res.send(result);
+  })
+});
+
+app.get('/api/videos', (req, res) => {
+  db.Video.findAll()
   .then(result => {
     res.send(result);
   })
