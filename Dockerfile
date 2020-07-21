8bfbe144e265aa96a933526896d6404d605034c6
Kana-E <kana.eguchi@yahoo.com>
2020-07-21 09:57:25 -0400

    adds Dockerfile and docker ignore

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# specify the node base image with your desired version node:<version>
+FROM node:10.15.3
+
+# Set the working directory
+WORKDIR /sdc
+
+# Copy root directory into docker root directory
+COPY . .
+
+# Command to run upon mounting image
+RUN npm install
+
+#application's default port
+EXPOSE 3002
+
+#specifies what command to run within the container
+CMD ["npm", "start", "npm", "build"]
--- database/postgres/index.js
+++ database/postgres/index.js
@@ -1,7 +1,6 @@
 const { Pool, Client } = require('pg')
 const pg_config = require('./pgConfig.json')
-// const dbName = require('./pgSetup.js')
-// const dataGenerator = require('./dataGenerator.js')
+// const {dbName} = require('./pgSetup.js')
 const path = require('path');
 
 //////////////////// Setup ///////////////////////
@@ -18,17 +17,13 @@ const path = require('path');
 const client = new Client({
   user: pg_config.user,
   host: pg_config.host,
-  database: 'sdc',
+  // database: dbName.dbName,
   password: pg_config.password,
   port: pg_config.port
 })
 client.connect()
 
 //////Banner///////
-// pool.on('error', (err, query) => {
-//   console.error('Unexpected error on idle client', err)
-//   process.exit(-1)
-// })
 
 var getOneBanner = (req, res) => {
   var getFirstId = `SELECT * FROM BANNER WHERE ID=1`
@@ -36,15 +31,6 @@ var getOneBanner = (req, res) => {
   client.query(getFirstId)
   .then(result => res.send(result.rows))
   .catch(errr => console.log(err.stack))
-  // , (err, result) => {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-  //     res.send(result.rows)
-  //   }
-  // })
-  // client.end()
-  // .catch(err => console.log(err.stack))
 }
 
 var getBannerById = (req, res) => {
@@ -54,18 +40,28 @@ var getBannerById = (req, res) => {
   client.query(getBannerById)
   .then(result => res.send(result.rows))
   .catch(errr => console.log(err.stack))
-
-  // , (err, result) => {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-  //     console.log(result.rows)
-  //     res.send(result.rows)
-  //   }
-  // })
-  // client.end()
-  // .catch(err => console.log(err))
 }
+
+var getOneVid = (req, res) => {
+  var getFirstId = `SELECT * FROM VIDEO WHERE ID=1`
+  // client.connect()
+  client.query(getFirstId)
+  .then(result => res.send(result.rows))
+  .catch(errr => console.log(err.stack))
+  // // client.end()
+    // .then(() => client.end())
+}
+
+var getVidById = (req, res) => {
+  var id = req.params.videoId
+  var getVidById = `SELECT * FROM VIDEO WHERE ID=${id}`
+  // client.connect()
+  client.query(getVidById)
+  .then(result => res.send(result.rows))
+  .catch(errr => console.log(err.stack))
+  //   // client.end()
+}
+
 
 // var postBanners = (req, res) => {
 //   var data = dataGenerator.bannerData()
@@ -79,45 +75,6 @@ var getBannerById = (req, res) => {
 //     }
 //   })
 // }
-
-var getOneVid = (req, res) => {
-  var getFirstId = `SELECT * FROM VIDEO WHERE ID=1`
-  // client.connect()
-  client.query(getFirstId)
-  .then(result => res.send(result.rows))
-  .catch(errr => console.log(err.stack))
-  //  (err, result) => {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-  //     res.send(result.rows)
-  //   }
-  // })
-  // // client.end()
-    // .then(() => client.end())
-    // .catch(err => console.log(err))
-}
-
-var getVidById = (req, res) => {
-  var id = req.params.videoId
-  console.log(id)
-  var getVidById = `SELECT * FROM VIDEO WHERE ID=${id}`
-  // client.connect()
-  client.query(getVidById)
-  .then(result => res.send(result.rows))
-  .catch(errr => console.log(err.stack))
-
-  // , (err, result) => {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-  //     res.send(result.rows)
-  //   }
-  //   // client.end()
-  // })
-  // .catch(err => console.log(err))
-}
-
 
 module.exports.getOneBanner = getOneBanner;
 module.exports.getBannerById = getBannerById;
